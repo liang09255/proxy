e5686c3b3ba7df7a166103789fdec686faf613a8
landryliang <landryliang@xiaoe-tech.com>
2024-03-15 18:22:13 +0800

    稍微整理下我的屎山

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"golang.org/x/net/ipv4"
@@ -21,8 +22,10 @@ const (
 	ProtocolUDP  = 17
 )
 
+var tun2 tun.Device
+
 func main() {
-	tun2 := initTun2("Test Tun", "20.0.0.1/24")
+	tun2 = initTun2("Test Tun", "20.0.0.1/24")
 	for {
 		// 第一个字节获取到协议版本号和头部长度
 		buf := make([]byte, 2048)
@@ -56,130 +59,20 @@ func main() {
 				tcp, _ := tl.(*layers.TCP)
 				log.Println("源端口:", tcp.SrcPort, "目的端口:", tcp.DstPort)
 				if tcp.SYN {
-					log.Println("第一次握手", tcp.Seq)
-					log.Println("第二次握手")
-
-					ip2 := &layers.IPv4{
-						Version:  4,
-						TTL:      255,
-						Flags:    layers.IPv4DontFragment,
-						Protocol: layers.IPProtocolTCP,
-						SrcIP:    ip.DstIP,
-						DstIP:    ip.SrcIP,
-					}
-
-					tcp2 := &layers.TCP{
-						SrcPort: tcp.DstPort,
-						DstPort: tcp.SrcPort,
-						SYN:     true,
-						ACK:     true,
-						Ack:     tcp.Seq + 1,
-						Seq:     123,
-						Window:  65535,
-					}
-					options := gopacket.SerializeOptions{
-						FixLengths:       true,
-						ComputeChecksums: true,
-					}
-					err = tcp2.SetNetworkLayerForChecksum(ip)
+					err = firstHandshake(ip, tcp)
 					if err != nil {
 						log.Println(err)
 						continue
 					}
-					buffer := gopacket.NewSerializeBuffer()
-					err := gopacket.SerializeLayers(buffer, options, ip2, tcp2)
-					if err != nil {
-						log.Println("Error serializing packet:", err)
-						continue
-					}
-					n2, err := tun2.Write(buffer.Bytes(), 0)
-					log.Println(buffer.Bytes())
-					if err != nil {
-						log.Println(err)
-						continue
-					}
-
-					log.Println(n2)
 				} else if tcp.ACK && !tcp.PSH {
 					log.Println("第三次握手", tcp.ACK, tcp.Seq)
 				} else if tcp.ACK && tcp.PSH {
 					log.Println("开始推送数据")
-					dialer := getSocks5TCPDialer2()
-					re := regexp.MustCompile(`(\d+)`) // 匹配数字
-					match := re.FindStringSubmatch(tcp.DstPort.String())
-					addr := ip.DstIP.String() + ":" + match[1]
-					conn, err := dialer.Dial("tcp", addr)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					log.Println("代理连接成功")
-
-					resp := make([]byte, 2048)
-					wg := sync.WaitGroup{}
-					wg.Add(2)
-					log.Println("传输数据ing")
-					go func() {
-						defer wg.Done()
-						n1, err := conn.Write(tcp.Payload)
-						if err != nil {
-							log.Println(err)
-						}
-						log.Println("发送了", n1)
-					}()
-					go func() {
-						defer wg.Done()
-						n2, err := conn.Read(resp)
-						if err != nil {
-							log.Println(err)
-						}
-						log.Println("接收到", n2)
-						resp = resp[:n2]
-					}()
-					wg.Wait()
-					log.Println("传输完成")
-					// 封装成ip包写回去
-					ip2 := &layers.IPv4{
-						Version:  4,
-						TTL:      255,
-						Flags:    layers.IPv4DontFragment,
-						Protocol: layers.IPProtocolTCP,
-						SrcIP:    ip.DstIP,
-						DstIP:    ip.SrcIP,
-					}
-
-					tcp2 := &layers.TCP{
-						SrcPort: tcp.DstPort,
-						DstPort: tcp.SrcPort,
-						PSH:     true,
-						ACK:     true,
-						Ack:     tcp.Seq + uint32(len(resp)),
-						Seq:     tcp.Ack,
-						Window:  65535,
-					}
-					options := gopacket.SerializeOptions{
-						FixLengths:       true,
-						ComputeChecksums: true,
-					}
-					err = tcp2.SetNetworkLayerForChecksum(ip)
+					err = proxyPacket(ip, tcp)
 					if err != nil {
 						log.Println(err)
 						continue
 					}
-					buffer := gopacket.NewSerializeBuffer()
-					err = gopacket.SerializeLayers(buffer, options, ip2, tcp2, gopacket.Payload(resp))
-					if err != nil {
-						log.Println(err)
-						continue
-					}
-					n2, err := tun2.Write(buffer.Bytes(), 0)
-					log.Println(buffer.Bytes())
-					if err != nil {
-						log.Println(err)
-						continue
-					}
-
-					log.Println(n2)
 				}
 			case ProtocolUDP:
 			default:
@@ -216,4 +109,114 @@ func getSocks5TCPDialer2() proxy.Dialer {
 		panic(err)
 	}
 	return dialer
+}
+
+func firstHandshake(ip *layers.IPv4, tcp *layers.TCP) (err error) {
+	log.Println("第一次握手", tcp.Seq)
+	log.Println("第二次握手")
+	ip2 := &layers.IPv4{
+		Version:  4,
+		TTL:      255,
+		Flags:    layers.IPv4DontFragment,
+		Protocol: layers.IPProtocolTCP,
+		SrcIP:    ip.DstIP,
+		DstIP:    ip.SrcIP,
+	}
+
+	tcp2 := &layers.TCP{
+		SrcPort: tcp.DstPort,
+		DstPort: tcp.SrcPort,
+		SYN:     true,
+		ACK:     true,
+		Ack:     tcp.Seq + 1,
+		Seq:     123,
+		Window:  65535,
+	}
+	options := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	err = tcp2.SetNetworkLayerForChecksum(ip)
+	if err != nil {
+		return err
+	}
+	buffer := gopacket.NewSerializeBuffer()
+	err = gopacket.SerializeLayers(buffer, options, ip2, tcp2)
+	if err != nil {
+		return fmt.Errorf("error serializing packet: %s", err.Error())
+	}
+	_, err = tun2.Write(buffer.Bytes(), 0)
+	log.Println(buffer.Bytes())
+	return err
+}
+
+func proxyPacket(ip *layers.IPv4, tcp *layers.TCP) (err error) {
+	dialer := getSocks5TCPDialer2()
+	re := regexp.MustCompile(`(\d+)`) // 匹配数字
+	match := re.FindStringSubmatch(tcp.DstPort.String())
+	addr := ip.DstIP.String() + ":" + match[1]
+	conn, err := dialer.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+	log.Println("代理连接成功")
+
+	resp := make([]byte, 2048)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	log.Println("传输数据ing")
+	go func() {
+		defer wg.Done()
+		n1, err := conn.Write(tcp.Payload)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("发送了", n1)
+	}()
+	go func() {
+		defer wg.Done()
+		n2, err := conn.Read(resp)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("接收到", n2)
+		resp = resp[:n2]
+	}()
+	wg.Wait()
+	log.Println("传输完成")
+	// 封装成ip包写回去
+	ip2 := &layers.IPv4{
+		Version:  4,
+		TTL:      255,
+		Flags:    layers.IPv4DontFragment,
+		Protocol: layers.IPProtocolTCP,
+		SrcIP:    ip.DstIP,
+		DstIP:    ip.SrcIP,
+	}
+
+	tcp2 := &layers.TCP{
+		SrcPort: tcp.DstPort,
+		DstPort: tcp.SrcPort,
+		PSH:     true,
+		ACK:     true,
+		Ack:     tcp.Seq + uint32(len(resp)),
+		Seq:     tcp.Ack,
+		Window:  65535,
+	}
+	options := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	err = tcp2.SetNetworkLayerForChecksum(ip)
+	if err != nil {
+		return err
+	}
+	buffer := gopacket.NewSerializeBuffer()
+	err = gopacket.SerializeLayers(buffer, options, ip2, tcp2, gopacket.Payload(resp))
+	if err != nil {
+		return err
+	}
+	_, err = tun2.Write(buffer.Bytes(), 0)
+	log.Println(buffer.Bytes())
+	return err
 }
